f54a48270177271390474907f274d46a26642658
Alex Olsen <alexolsen@Alexs-MacBook-Pro.local>
2016-12-10 19:58:51 -0600

    nav styling

--- imports/ui/components/navigation/navigation.js
+++ imports/ui/components/navigation/navigation.js
@@ -2,13 +2,38 @@ import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import templateUrl from './navigation.html';
 
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+
 class Navigation {
   constructor($scope, $reactive) {
     'ngInject';
 
     $reactive(this).attach($scope);
 
+    Meteor.subscribe('users');
+
     this.isUser = Meteor.userId() ? true : false;
+
+    this.helpers({
+        currentUser() {
+            if(!Meteor.user())
+                return '';
+
+            let user = Meteor.user();
+            return user.emails[0].address.split('@')[0];
+        }
+    })
+
+  }
+
+  name() {
+    if(Meteor.user())
+        return Meteor.user().emails[0].split('@')[0];
+  }
+
+  logout() {
+    Accounts.logout();
   }
 }
 
